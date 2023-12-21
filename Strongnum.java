f1693eeb8da2fdf8ba191fa1a2ee0a222b6474ce
Rashithselva <ravirashith@gmail.com>
2023-12-21 19:28:23 +0530

    add factorial program

--- Strongnum.java
+++ Strongnum.java
@@ -1,5 +1,15 @@
 package Program1;
 
 public class Strongnum {
+    public static void main(String[] args) {
+        
+        int a=5;
+        int fact=1;
+
+        for(int i=1;i<=a;i++){
+            fact*=i;
+        }
+        System.out.println(fact);
+    }
     
 }
