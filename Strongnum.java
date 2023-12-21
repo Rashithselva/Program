c8b58e5d3fc3ece94011f4b7cb6851221df53533
Rashithselva <ravirashith@gmail.com>
2023-12-21 19:38:09 +0530

    Strong number program added

--- Strongnum.java
+++ Strongnum.java
@@ -1,15 +1,30 @@
 package Program1;
+import java.util.*;
 
 public class Strongnum {
     public static void main(String[] args) {
+        Scanner r=new Scanner(System.in);
         
-        int a=5;
-        int fact=1;
+        System.out.println("Enter the number");
+        int num=r.nextInt();
+        int last=0,fact=1,sum=0,temp=num;
 
-        for(int i=1;i<=a;i++){
-            fact*=i;
+        while(num>0){
+            last=num%10;
+            for(int i=1;i<=last;i++){
+                fact*=i;
+            }
+            sum+=fact;
+            fact=1;
+            last=0;
+            num/=10;
         }
-        System.out.println(fact);
+        if(sum==temp){
+            System.out.println("Strong Number");
+        }
+        else{
+            System.out.println("Not Strong Number");
+        }
     }
     
 }
